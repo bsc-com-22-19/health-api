bc0c8bb419a069d50374796dcf6e12f3a1cf6b64
Bsc-com-22-19 <Bsc-com-22-19@unima.ac.mw>
2023-04-10 18:28:43 +0200

    Express server with error on browser

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const post = require('./src/posts/post.model')
+
+app.get('/api/v1', function(req, res) {
+    return res.json(req, res)
+})
+
+app.get('/api/v1/posts', function(req, res) {
+    return res.json([post])
+})
+
+app.listen(3000, function() {
+    console.log('HEALTH listening on port 3000')
+
+})
